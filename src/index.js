5dc9be5a587a4a3f51580f44c31dbb271596e96d
tone4ka <toniamik@gmail.com>
2020-10-17 17:24:10 +0300

    init: configCorrectionNeed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let br = "(){}[]";
+  let st = [];
+  
+  for (let i = 0; i < str.length; i++) {
+
+      let char = str[i];
+      let index = br.indexOf(char);
+      
+      if (index >= 0) {
+        
+          if (index & 1) {
+              
+              if (!st.length) return false;
+              
+              let last_br = st.pop();
+              
+              if (last_br != br[index - 1]) return false;
+          } else { 
+              
+              st.push(char);
+          }
+
+
+      }
+  }
+
+  return !st.length;
 }
